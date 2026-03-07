76ba661f9c005bb760a2406286c7c7ec8a75dfc4
Azat Kashapov <a.kashapov@rkeeper.ru>
2026-03-07 19:07:36 +0300

    better_thread_example

---
+++ Project/Project.Infrustucture/BetterThreadExample.cs
@@ -0,0 +1,29 @@
+﻿namespace Project.Infrastructure;
+
+public class BetterThreadExample
+{
+    private static int counter = 0;
+
+    public static void Run()
+    {
+        Action task = () =>
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                Interlocked.Increment(ref counter);
+            }
+        };
+
+
+        Thread thread1 = new Thread(new ThreadStart(task));
+        Thread thread2 = new Thread(new ThreadStart(task));
+
+        thread1.Start();
+        thread2.Start();
+      
+        thread1.Join();
+        thread2.Join();
+        
+        Console.WriteLine(counter);
+    }
+}
