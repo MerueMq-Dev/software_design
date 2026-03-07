254b43ef6228131eb4cfff636ee30d9360f65e0d
Azat Kashapov <a.kashapov@rkeeper.ru>
2026-03-07 21:41:28 +0300

    complex_multi_thread_processing

---
+++ Project/Project.Infrustucture/ComplexMultiThreadProcessing.cs
@@ -0,0 +1,34 @@
+﻿public class ComplexMultiThreadProcessing
+{
+    private static readonly int SIZE = 1_000_000;
+    private static readonly int THREADS = 4;
+    private static int sum = 0;
+
+    public static void Run()
+    {
+        var rand = new Random();
+        var data = Enumerable.Range(0, SIZE)
+            .Select(_ => rand.Next(100))
+            .ToArray();
+
+        int chunkSize = SIZE / THREADS;
+
+        var threads = Enumerable.Range(0, THREADS)
+            .Select(i => {
+                int start = i * chunkSize;
+                int end = (i == THREADS - 1) ? SIZE : start + chunkSize;
+                int[] chunk = data[start..end];
+                return new Thread(() =>
+                {
+                    int localSum = chunk.Sum();
+                    Interlocked.Add(ref sum, localSum);
+                });
+            })
+            .ToArray();
+
+        Array.ForEach(threads, t => t.Start());
+        Array.ForEach(threads, t => t.Join());
+
+        Console.WriteLine("Sum of all elements: " + sum);
+    }
+}
