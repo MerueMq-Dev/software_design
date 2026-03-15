567c1e6396578d39a142f551b8fb393f5ec65f37
Azat Kashapov <a.kashapov@rkeeper.ru>
2026-03-15 15:26:58 +0300

    hoare's_triples

---
+++ Project/Project.Infrustucture/HoareTriples.cs
@@ -0,0 +1,20 @@
+﻿namespace Project.Infrastructure;
+
+public static class HoareTriples
+{
+    // {P: number — любое целое} Abs(number)
+    // {Q: result >= 0 && result == |number|}
+    private static int Abs(int number) =>
+        number >= 0 ? number : -number;
+
+    // {P: firstNumber, secondNumber — любые целые} Max(firstNumber, secondNumber)
+    // {Q: result >= firstNumber && result >= secondNumber
+    //  && (result == firstNumber || result == secondNumber)}
+    private static int Max(int firstNumber, int secondNumber) =>
+        firstNumber < secondNumber ? secondNumber : firstNumber;
+
+    // {P: firstNumber, secondNumber — любые целые} MaxAbs(firstNumber, secondNumber)
+    // {Q: result == Max(Abs(firstNumber), Abs(secondNumber)) && result >= 0}
+    public static int MaxAbs(int firstNumber, int secondNumber) =>
+        Max(Abs(firstNumber), Abs(secondNumber));
+}
