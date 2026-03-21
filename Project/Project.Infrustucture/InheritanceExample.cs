1c070e6b7c56ae1e487c328fc80119844612be82
Azat Kashapov <a.kashapov@rkeeper.ru>
2026-03-22 00:00:41 +0300

    inheritance_example

---
+++ Project/Project.Infrustucture/InheritanceExample.cs
@@ -0,0 +1,30 @@
+﻿namespace Project.Infrastructure;
+
+// В этом случае выведется "Meow" так как переменная animal хоть и является типом Animal но всё ещё хранит ссылку на объект типа Cat
+
+public class InheritanceExample
+{
+    public static void Run()
+    {
+        Animal animal = new Cat();
+        animal.makeSound();
+    }
+}
+
+public abstract class Animal
+{
+    public virtual void makeSound()
+    {
+        Console.WriteLine("Some generic animal sound");
+    }
+}
+
+class Cat : Animal
+{
+    public override void makeSound()
+    {
+        Console.WriteLine("Meow");
+    }
+}   
+
+
